53619901856659c062488caf89463c9b9b0b3657
user <rifatikmolodets@mail.ru>
2025-03-20 23:55:27 +0400

    hw 6

--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('products/', views.list_products, name='list_products'),
     path('orders/', views.list_orders, name='list_orders'),
     path('upload/', views.upload_file, name='upload_file'),
+    path('products/create/', views.create_product, name='create_products'),
+    path('orders/create/', views.create_order, name='create_order'),
 ]
